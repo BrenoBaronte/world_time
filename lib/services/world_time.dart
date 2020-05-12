ba8daad1a1b9600bee540886bf48ec35c1bd92d4
Breno Baronte <brenobaronte@hotmail.com>
2020-05-11 21:20:17 -0300

    Lesson 30

---
+++ lib/services/world_time.dart
@@ -0,0 +1,36 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+
+  String location; // location name for the UI
+  String time; // time in that location
+  String flag; // url to an asset flag icon
+  String url; // location url for api endpoint
+
+  WorldTime({ this.location, this.flag, this.url });
+
+  Future<void> getTime() async {
+
+    try {
+      // make the request
+      Response response = await get('https://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+
+      // get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+
+      // create datetime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      // set the time property
+      time = now.toString();
+    }
+    catch (e) {
+      print('caught error: $e');
+      time = 'could not get time data';
+    }
+  }
+}
